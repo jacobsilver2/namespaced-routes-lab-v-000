f35ad8b4419012fabf645f7547f6e874c80a355b
jacobsilver2 <jacobsilver2@mac.com>
2018-04-02 13:13:26 -0400

    dfdf

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,6 +6,9 @@ class SongsController < ApplicationController
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
         redirect_to artists_path, alert: "Artist not found"
+      elsif @preferences && @preferences.song_sort_order
+        @artist.songs.order(name: @preferences.song_sort_order)
+        
       else
         @songs = @artist.songs
       end
